b7975ef45442e6568e8821e38c13cc63cad4b9a3
Seth Manesse <scmanesse@gmail.com>
2018-11-12 20:27:59 -0700

    refactored based on some older code I found from a previous script I wrote

--- src/Driver.java
+++ src/Driver.java
@@ -1,72 +1,66 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    public static WebDriver b;
-    public static final long OPENS_AT = 1542250800;
-    public static final String[] crns = {"10816", "13803", "10172", "11402", "12663", "10166", "13128"};
-    public static final String semester = "Spring 2019";
-
     public static void main(String[] args) {
+        String[] crns = {"asdf", "asdfdas", "asdf", "asdf", "asfd", "asdf", "sdaf"};
+        String semester = "Spring 2018";
         System.setProperty("webdriver.gecko.driver", System.getProperty("path"));
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "null");
-        b = new FirefoxDriver();
-        b.get("https://ss.banner.usu.edu/StudentRegistrationSsb/ssb/term/termSelection?mode=registration");
-        b.findElement(By.id("registerLink")).click();
+        WebDriver b = new FirefoxDriver();
+        b.get("https://my.usu.edu/");
         b.findElement(By.name("username")).sendKeys(System.getProperty("username"));
         b.findElement(By.name("password")).sendKeys(System.getProperty("password"));
-        b.findElement(By.name("submit")).click();
+        b.findElement(By.xpath("//input[@value='LOGIN']")).click();
+        wait(10); //for duo
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Register for Classes")));
+//        b.get(b.findElement(By.linkText("Register for Classes")).getAttribute("href"));
 
-        //click the select semester
-        b.findElement(By.className("select2-choice")).click();
+        b.get("https://ss.banner.usu.edu/StudentRegistrationSsb/ssb/registration/registerPostSignIn?mode=registration");
+        b.findElement(By.id("s2id_txt_term")).click();
+        String xpath = "//body//*[text()[contains(.,'" + semester + "')]]";
+        WebDriverWait wait = new WebDriverWait(b, 3);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        b.findElement(By.xpath(xpath)).click();
 
-        //wait until spring is available
-        while (true) {
-            try {
-                b.findElement(By.xpath("//div[text()[contains(.,'" + semester + "')]]")).click();
-                break;
-            } catch (WebDriverException e) {
-                System.out.println("caught");
-                long seconds = OPENS_AT - (System.currentTimeMillis() / 1000);
-                System.out.println(seconds);
-                if (seconds < 15) {
-                    //don't wait
-                } else if (seconds < 60) { // if less than 1 minute, try every second
-                    wait(1000);
-                } else if (seconds < 120) { //if less than 2 minutes, try every 5 seconds
-                    wait(5000);
-                } else if (seconds < 300) { //if less than 5 minutes, try every 10 seconds
-                    wait(10000);
-                } else if (seconds < 1200) { //if less than 10 minutes, try every 15 seconds
-                    wait(15000);
-                } else if (seconds < 900) { //if less than 20 minutes, try every 30 seconds
-                    wait(30000);
-                } else if (seconds < 1800) {//if less than 30 minutes, try every 45 seconds
-                    wait(45000);
-                } else {
-                    wait(60000); //otherwise wait a minute
-                }
-                b.get(b.getCurrentUrl());
-            }
-        }
+        b.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        refresh(b, semester);
 
-        b.findElement(By.id("term-go")).click();
         b.findElement(By.id("enterCRNs-tab")).click();
-//        b.findElement(By.id("txt_crn1")).sendKeys(crns[0]);
-        for (int i = 1; i < crns.length; i++) {
+
+        for (int i = 1; i <= crns.length; i++) {
+            b.findElement(By.id("txt_crn" + i)).sendKeys(crns[i - 1]);
+            if (i == crns.length) {
+                break;
+            }
             b.findElement(By.id("addAnotherCRN")).click();
-            b.findElement(By.id("txt_crn" + (i + 1))).sendKeys(crns[i]);
         }
 //        b.findElement(By.id("addCRNbutton")).click();
-//        b.findElement(By.id("saveButton")).click();
-
 
     }
 
-    public static void wait(int milliseconds) {
+    public static void refresh(WebDriver b, String semester) {
         try {
-            Thread.sleep(milliseconds);
+            b.findElement(By.id("enterCRNs-tab")).click();
+        } catch (WebDriverException e) {
+            b.findElement(By.xpath("//button[text()='Ok']")).click();
+            String xpath = "//body//*[text()[contains(.,'" + semester + "')]]";
+            b.findElement(By.xpath(xpath)).click();
+            refresh(b, semester);
+        }
+    }
+
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
