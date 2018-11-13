6577c1ba2576fe56a72fc5af38500fb31fb05ff9
Seth Manesse <scmanesse@gmail.com>
2018-11-12 20:42:31 -0700

    working commit

--- src/Driver.java
+++ src/Driver.java
@@ -11,7 +11,8 @@ import java.util.concurrent.TimeUnit;
 public class Driver {
     public static void main(String[] args) {
         String[] crns = {"asdf", "asdfdas", "asdf", "asdf", "asfd", "asdf", "sdaf"};
-        String semester = "Spring 2018";
+//        String semester = "Spring 2019";
+        String semester = "Fall 2018";
         System.setProperty("webdriver.gecko.driver", System.getProperty("path"));
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "null");
@@ -20,19 +21,22 @@ public class Driver {
         b.findElement(By.name("username")).sendKeys(System.getProperty("username"));
         b.findElement(By.name("password")).sendKeys(System.getProperty("password"));
         b.findElement(By.xpath("//input[@value='LOGIN']")).click();
-        wait(10); //for duo
+//        wait(10); //for duo
 //        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Register for Classes")));
 //        b.get(b.findElement(By.linkText("Register for Classes")).getAttribute("href"));
 
         b.get("https://ss.banner.usu.edu/StudentRegistrationSsb/ssb/registration/registerPostSignIn?mode=registration");
+//        String xpath = "//body//*[text()[contains(.,'" + semester + "')]]";
+//        WebDriverWait wait = new WebDriverWait(b, 3);
+//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+//        b.findElement(By.xpath(xpath)).click();
+//        b.findElement(By.id("term-go"));
         b.findElement(By.id("s2id_txt_term")).click();
         String xpath = "//body//*[text()[contains(.,'" + semester + "')]]";
-        WebDriverWait wait = new WebDriverWait(b, 3);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
         b.findElement(By.xpath(xpath)).click();
 
-        b.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        refresh(b, semester);
+//        b.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        refresh(b);
 
         b.findElement(By.id("enterCRNs-tab")).click();
 
@@ -47,20 +51,24 @@ public class Driver {
 
     }
 
-    public static void refresh(WebDriver b, String semester) {
+    public static void refresh(WebDriver b) {
         try {
             b.findElement(By.id("enterCRNs-tab")).click();
-        } catch (WebDriverException e) {
-            b.findElement(By.xpath("//button[text()='Ok']")).click();
-            String xpath = "//body//*[text()[contains(.,'" + semester + "')]]";
-            b.findElement(By.xpath(xpath)).click();
-            refresh(b, semester);
+        } catch (WebDriverException f) {
+            try {
+                b.findElement(By.xpath("//button[text()='Ok']")).click();
+            } catch (org.openqa.selenium.NoSuchElementException e) {
+                //catch for first case
+            }
+            b.findElement(By.id("term-go")).click();
+            wait(500);
+            refresh(b);
         }
     }
 
-    public static void wait(int seconds) {
+    public static void wait(int milli) {
         try {
-            Thread.sleep(seconds * 1000);
+            Thread.sleep(milli);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
